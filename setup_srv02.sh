212adebe44583bd1fc6efacabcdefdc0cb8c488f
Most Wanted Duck <81170836+mostwantedduck@users.noreply.github.com>
2023-05-22 14:10:17 +0200

    Update setup_srv02.sh

--- setup_srv02.sh
+++ setup_srv02.sh
@@ -31,7 +31,7 @@ echo "$config_content" | tee /etc/nginx/http.d/default > /dev/null
 
 echo "Nginx configuration has been updated and the service has been restarted."
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /tmp/srv02.key -out /tmp/srv02.crt -config /tmp/srv02.conf
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /tmp/srv02.key -out /tmp/srv02.crt
 
 mv /tmp/srv02.crt /etc/ssl/certs/srv02.crt
 mv /tmp/srv02.key /etc/ssl/private/srv02.key
