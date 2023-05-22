0d47f986d4b3e63d326c33190d07c35c723b2fa1
Most Wanted Duck <81170836+mostwantedduck@users.noreply.github.com>
2023-05-22 15:05:37 +0200

    Update setup_srv02.sh

--- setup_srv02.sh
+++ setup_srv02.sh
@@ -1,12 +1,15 @@
 #!/bin/ash
 
-apk add nginx nano nftables
+apk add nginx nano iptables
 
-nft add table inet filter
-nft add chain inet filter input { type filter hook input priority 0 \; }
-nft add rule inet filter input tcp dport 22 accept
-nft add rule inet filter input tcp dport 80 accept
-nft add rule inet filter input tcp dport 443 accept
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+/etc/init.d/iptables save
+/etc/init.d/iptables restart
+
+
 
 config_content=$(cat <<'EOF'
 server {
