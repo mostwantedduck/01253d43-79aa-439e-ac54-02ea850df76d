1a042805b8c22d713b40581bc8bed624b9b7acc3
Most Wanted Duck <81170836+mostwantedduck@users.noreply.github.com>
2023-05-27 19:17:34 +0200

    Update setup_srv02.sh

--- setup_srv02.sh
+++ setup_srv02.sh
@@ -7,31 +7,9 @@ echo "Configuring the firewall"
 # Flush all existing rules:
 iptables -F
 
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT DROP
-
-# Set the default policy to DROP:
-#iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-#iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-# Allow established connections:
-#iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-#iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-# Allow localhost traffic:
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
-
-# Allow the required ports:
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
-
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 80 -j ACCEPT
-
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 443 -j ACCEPT
 
 iptables-save > /etc/iptables/rules.v4
 iptables-save
