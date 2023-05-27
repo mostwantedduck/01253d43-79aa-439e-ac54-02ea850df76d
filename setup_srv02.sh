fedffbc740cff6426844eef5d8a90da96d9210b0
Most Wanted Duck <81170836+mostwantedduck@users.noreply.github.com>
2023-05-27 20:38:37 +0200

    Update setup_srv02.sh

--- setup_srv02.sh
+++ setup_srv02.sh
@@ -4,15 +4,14 @@ apk add nginx nano iptables curl
 
 echo "Configuring the firewall"
 
-# Flush all existing rules:
 iptables -F
 
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8080 -j DROP
 
-iptables-save > /etc/iptables/rules.v4
-iptables-save
+/etc/init.d/iptables save
 
 echo "Configuring the nginx"
 
